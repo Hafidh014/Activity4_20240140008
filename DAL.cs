04eae6ff5428086da04e1a4771552d3d65832e85
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-06-20 11:35:37 +0700

    reset data

--- DAL.cs
+++ DAL.cs
@@ -223,3 +223,28 @@ namespace CRUDmahasiswaADO
                 if (conn.State == ConnectionState.Open) conn.Close();
             }
         }
+
+        public void resetData()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                string deleteQuery = "DELETE FROM mahasiswa;";
+                SqlCommand cmdDelete = new SqlCommand(deleteQuery, conn);
+                cmdDelete.ExecuteNonQuery();
+
+                string insertQuery = @"
+                    INSERT INTO mahasiswa
+                    SELECT * FROM mahasiswa_backup;";
+                SqlCommand cmdInsert = new SqlCommand(insertQuery, conn);
+                cmdInsert.ExecuteNonQuery();
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open) conn.Close();
+            }
+        }
